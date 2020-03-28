2e4543a9e5530713672798007192ab3df94cde49
Jon Jazzy <jonjazzy123@gmail.com>
2020-03-27 19:55:41 -0600

    CRUD for JDBC
    public void run(String... args) throws Exception {
    		LOGGER.info("All Users --> {}", dao.findAll());
    //		LOGGER.info("User Id 10001 --> {}", dao.findById(10001));
    //		LOGGER.info("User with Name = James --> {}", dao.findByName("James"));
    //		LOGGER.info("Delete Person with Id 10001 --> ", dao.deleteById(10001));
    
    		LOGGER.info("Insert the Person 10020 --> {}",
    				dao.insertPerson(new Person(10020, "Jannette", "Tokyo", new Date()))
    		);
    
    		LOGGER.info("Update the Person 10003 --> {}",
    				dao.updatePerson(new Person(10003, "Pieter", "Eintracht", new Date()))
    		);
    
    //select * from person
        public List<Person> findAll()
        {
            return jdbcTemplate.query("SELECT * FROM Person",
                            new BeanPropertyRowMapper<Person>(Person.class));
        }
    
        //select * from person WHERE id = {$id}
        public Person findById(int id)
        {
            return jdbcTemplate.queryForObject(
                    "SELECT * FROM Person WHERE id=?",
                    new Object[]{id},
                    new BeanPropertyRowMapper<Person>(Person.class)
            );
        }
    
        //select * from person WHERE name = {$name}
        public List<Person> findByName(String name)
        {
            return jdbcTemplate.query(
                    "SELECT * FROM Person WHERE name=?",
                    new Object[]{name},
                    new BeanPropertyRowMapper<Person>(Person.class)
            );
        }
    
        //delete * from person WHERE id = {$id}
        public int deleteById(int id)
        {
            return jdbcTemplate.update(
                    "DELETE FROM Person WHERE id=?",
                    new Object[] {id}
            );
        }
    
        //INSERT into table
        public int insertPerson(Person person)
        {
            return jdbcTemplate.update(
                    "INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE ) VALUES(?, ?, ?, ?);",
                    new Object[] {
                            person.getId(),
                            person.getName(),
                            person.getLocation(),
                            new Timestamp(person.getBirthDate().getTime())}
            );
        }
    
        //UPDATE a person
        public int updatePerson(Person person)
        {
            return jdbcTemplate.update(
                    "UPDATE PERSON SET name=?, location=?, birth_date=? WHERE id=?;",
                    new Object[] {
                            person.getName(),
                            person.getLocation(),
                            new Timestamp(person.getBirthDate().getTime()),
                            person.getId()
                    }
            );
        }

--- database-demo/src/main/java/com/jonjazzy/databasedemo/DatabaseDemoApplication.java
+++ database-demo/src/main/java/com/jonjazzy/databasedemo/DatabaseDemoApplication.java
@@ -1,5 +1,6 @@
 package com.jonjazzy.databasedemo;
 
+import com.jonjazzy.databasedemo.entity.Person;
 import com.jonjazzy.databasedemo.jdbc.PersonJdbcDOA;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class DatabaseDemoApplication implements CommandLineRunner {
@@ -22,5 +25,16 @@ public class DatabaseDemoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		LOGGER.info("All Users --> {}", dao.findAll());
+//		LOGGER.info("User Id 10001 --> {}", dao.findById(10001));
+//		LOGGER.info("User with Name = James --> {}", dao.findByName("James"));
+//		LOGGER.info("Delete Person with Id 10001 --> ", dao.deleteById(10001));
+
+		LOGGER.info("Insert the Person 10020 --> {}",
+				dao.insertPerson(new Person(10020, "Jannette", "Tokyo", new Date()))
+		);
+
+		LOGGER.info("Update the Person 10003 --> {}",
+				dao.updatePerson(new Person(10003, "Pieter", "Eintracht", new Date()))
+		);
 	}
 }
--- database-demo/src/main/java/com/jonjazzy/databasedemo/jdbc/PersonJdbcDOA.java
+++ database-demo/src/main/java/com/jonjazzy/databasedemo/jdbc/PersonJdbcDOA.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -19,5 +20,62 @@ public class PersonJdbcDOA
     {
         return jdbcTemplate.query("SELECT * FROM Person",
                         new BeanPropertyRowMapper<Person>(Person.class));
+    }
+
+    //select * from person WHERE id = {$id}
+    public Person findById(int id)
+    {
+        return jdbcTemplate.queryForObject(
+                "SELECT * FROM Person WHERE id=?",
+                new Object[]{id},
+                new BeanPropertyRowMapper<Person>(Person.class)
+        );
+    }
+
+    //select * from person WHERE name = {$name}
+    public List<Person> findByName(String name)
+    {
+        return jdbcTemplate.query(
+                "SELECT * FROM Person WHERE name=?",
+                new Object[]{name},
+                new BeanPropertyRowMapper<Person>(Person.class)
+        );
+    }
+
+    //delete * from person WHERE id = {$id}
+    public int deleteById(int id)
+    {
+        return jdbcTemplate.update(
+                "DELETE FROM Person WHERE id=?",
+                new Object[] {id}
+        );
+    }
+
+
+    //INSERT into table
+    public int insertPerson(Person person)
+    {
+        return jdbcTemplate.update(
+                "INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE ) VALUES(?, ?, ?, ?);",
+                new Object[] {
+                        person.getId(),
+                        person.getName(),
+                        person.getLocation(),
+                        new Timestamp(person.getBirthDate().getTime())}
+        );
+    }
+
+    //UPDATE a person
+    public int updatePerson(Person person)
+    {
+        return jdbcTemplate.update(
+                "UPDATE PERSON SET name=?, location=?, birth_date=? WHERE id=?;",
+                new Object[] {
+                        person.getName(),
+                        person.getLocation(),
+                        new Timestamp(person.getBirthDate().getTime()),
+                        person.getId()
+                }
+        );
     }
 }
